662cba210321ba9c7c8c16927a2d98ab9e9fb393
unknown <cbkarwa@gmail.com>
2021-01-07 18:57:02 +0530

    First

--- src/public/script.js
+++ src/public/script.js
@@ -5,18 +5,45 @@ const socket = io();
 const usersList = document.querySelector(".users-name");
 const chatForm = document.getElementById("message-form");
 const messageInput = document.querySelector("#msg");
-const messages = document.querySelector(".messages");
+const messages = document.querySelector(".messages"); 
 
 // Getting username from index.html, here qs is a library to parse querystring in url 
 const {username} = Qs.parse(location.search, {ignoreQueryPrefix: true});
+
+
 
 /////////////////////// IMPLEMENT BELOW STEPS //////////////////////
 
 // Send username about "userJoin" to server 
 
+socket.emit("userJoin", username);
+
 // Listen for "updateUsers" from server and update usersList with new list of users, each user should be a li element containing username.
+
+socket.on("updateUsers", (users) => {
+  usersList.innerHTML = "";
+  for(let i=0; i<users.length; i++){
+    const li = document.createElement("li");
+    li.innerHTML = `${users[i].username}`;
+    document.querySelector(".users-name").appendChild(li);
+  }
+});
 
 // Listen for "message" from server and add new msg to messages, each message is a div element with class "message" 
 // containing 2 paragraphs, one with class "meta" containing username & other with class "text" containing message.
+socket.on('message', msg=>{
+  const NewDiv = document.createElement('div');
+  NewDiv.innerHTML = 
+  `<p class="meta">${msg.username}</p>
+  <p class="text">${msg.message}</p>`;
+  NewDiv.classList.add("message");
+  messages.appendChild(NewDiv);
+  messages.scrollTop = messages.scrollHeight;
+})
 
 // When a user submit a message in chatForm send {username: username, message: messageInput.value } about chatMessage to server 
+chatForm.addEventListener('submit', (e)=>{
+  e.preventDefault();
+  socket.emit('chatMessage', {username: username, message: inputField.value });
+  inputField.value = "";
+})
--- src/server.js
+++ src/server.js
@@ -1,37 +1,43 @@
-// importing required modules
 const express = require('express');
 const path = require('path');
-require('dotenv').config();
-
-// creating an express app, then an http server, which is then passed to socketio
 const app = express();
 const http = require('http').createServer(app);
 const io = require("socket.io")(http);
-
-// This will contain all the users
+require('dotenv').config();
 const users = [];
 
-// Setting up of port 
 const port = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'public')));
 
-/////////////////////// IMPLEMENT BELOW STEPS //////////////////////
+io.on('connection', socket => {
 
-// Setup io to listen to new connection and then inside its callback implement
+  socket.emit('message', { username: "Bot", message: "Welcome to chatbox" });
 
-  // Send {username:"Bot", message:"Welcome to chatbox"} about "message" to current socket only
+  // Getting details of new user from client
+  socket.on("userJoin", username => {
+    users.push({ id: socket.id, username: username });
+    socket.broadcast.emit('message', { username: "Bot", message: `${username} has joined the chat` });
+    io.emit("updateUsers", users);
+  });
 
-  // Listen for "userJoin" from client to get new username, add him to users array as {id: socket.id, username: username},
-  // send {username:"Bot",message:`${username} has joined the chat`} about "message" to everyone except current socket and
-  // send updated users array about "updateUsers" to every socket
+  // when the socket disconnets
+  socket.on('disconnect', () => {
+    const index = users.findIndex(user => user.id === socket.id);
+    socket.broadcast.emit('message', { username: "Bot", message: `${users[index] != null ? users[index].username : null} has left the chat` });
+    users.splice(index, 1);
+    io.emit("updateUsers", users);
+  });
 
-  // Listen for "disconnect", find the username from users array matching socket.id and 
-  // send {username:"Bot",message:`${username} has left the chat`} about "message" to everyone except current socket
-  // also remove the user from users array send updated users array about "updateUsers" to every socket
+  //  Getting chat messages of user from client
+  socket.on("chatMessage", msg => {
+    io.emit('message', { username: msg.username, message: msg.message });
+  });
 
-  // Listen for "chatMessage" for any message and send {username:msg.username,message:msg.message} about "message" to every socket
+});
 
 
 let server = http.listen(port, () => console.log(`Server Running at port ${port}`));
+
 
 if (process.env.NODE_ENV === 'testing') {
   console.log('will kill the server after 30sec')
